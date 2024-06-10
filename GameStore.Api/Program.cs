bae6dd7d877758fe9afde7711b1b2447392cf8f8
Ashique S <ashiques12@gmail.com>
2024-06-10 13:41:26 +1000

    Closes #3

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetRoute = "GetGame";
+
 List<GameDto> games = [
     new GameDto(1,"FIFA 23", "Sports",100M,new DateOnly(2022,9,10)),
     new GameDto(2,"Super Mario","RPG",99.99M,new DateOnly(2018,9,10)),
@@ -16,7 +18,47 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/games", () => games);
 
 // GET Game by ID
-app.MapGet("/games/{id}", (int id) => games.Find(x => x.Id == id));
+app.MapGet("/games/{id}", (int id) => games.Find(x => x.Id == id)).WithName(GetRoute);
+
+
+// POST Game
+app.MapPost("/games", (CreateGameDto createGameDto) =>
+{
+    // add into Game Object
+    GameDto game = new(
+        games.Count + 1,
+        createGameDto.Name,
+        createGameDto.Genre,
+        createGameDto.Price,
+        createGameDto.ReleaseDate
+    );
+    // adding into games list
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetRoute, new { id = game.Id }, game);
+
+});
+
+// PUT Game
+app.MapPut("/games/{id}", (int id, UpdateGameDto updateGameDto) =>
+{
+    //Find the object by Id
+    var index = games.FindIndex(x => x.Id == id);
+    if (index == -1)
+    {
+        Results.NotFound();
+    }
+    games[index] = new(
+        id,
+        updateGameDto.Name,
+        updateGameDto.Genre,
+        updateGameDto.price,
+        updateGameDto.ReleaseDate
+   );
+
+    return Results.NoContent();
+
+});
 
 //DELETE game by ID
 app.MapDelete("/games/{id}", (int id) =>
